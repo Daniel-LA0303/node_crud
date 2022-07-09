d3db05624435d42b9e8d9e5618f1362aa37fc7e0
Daniel-La0303 <03daniel030399@gmail.com>
2022-07-09 00:16:31 -0500

    add router getallcustomers, getonecustomer, updateCustomer, deleteCustomer and add their controllers

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -15,3 +15,57 @@ exports.newCustomer = async (req, res, next) => {
         next();
     }
 }
+
+//10. muestra todos los clientes
+exports.getCustomers = async(req, res, next) => {
+
+    try {
+        const customers = await Customers.find({});
+        res.json(customers)
+    } catch (error) {   
+        console.log(error);
+        next();
+    }
+}
+
+//12. muestra un cliente por su id
+exports.getOneCustomer = async(req, res, next) => {
+    try {
+        const customer = await Customers.findById(req.params.id);
+        //mostrar el cliente 
+        res.json(customer);
+        
+    } catch (error) {
+        console.log(error);
+        res.json({mensaje: 'Ese Cliente no existe'});
+        next();
+    }
+}
+//14. actualiza un ciente por su id
+exports.updateCustomer = async(req, res, next) => {
+    try {
+        const customer = await Customers.findOneAndUpdate(
+            {_id : req.params.id}, 
+            req.body, 
+            {new: true}
+        );
+        res.json(customer);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+//16. eliminar un cliente por id
+exports.deleteCustomer = async(req, res, next) => {
+    try {
+        await Customers.findOneAndDelete(
+            {_id : req.params.id}
+        )
+        res.json({mensaje: 'El cliente se ha eliminado'})
+    } catch (error) {
+        console.log(error);
+        next();
+
+    }
+}
+
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,27 @@ const router = express.Router();
 const customersController = require('../controllers/customersController');
 
 
-
 module.exports = function(){
 
     //6. Agrega nuevos clientes via Post
     router.post('/customers', 
             customersController.newCustomer);
- 
+
+    //9. obtener clientes
+    router.get('/customers/getAll',
+        customersController.getCustomers);
+
+    //11. muestra un cliente por id
+    router.get('/customers/:id',
+        customersController.getOneCustomer);
+
+    //13. Actualizar un cliente 
+    router.put('/customers/:id', 
+        customersController.updateCustomer);
+    
+    //15. eliminar un cliente por id
+    router.delete('/customers/:id', 
+        customersController.deleteCustomer);
+
     return router;
 }
